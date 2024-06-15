e2688dd6519319ea32414c2c5e4811ca6b60f3cc
kaioteoi <15633941+kaioteoi@users.noreply.github.com>
2024-06-15 02:17:48 -0300

    Enhance setup.sh and zshrc

--- setup.sh
+++ setup.sh
@@ -10,12 +10,39 @@ fi
 # Make sure we’re using the latest Homebrew.
 brew update
 
+brew install coreutils
 
 brew install zsh
 chsh -s $(which zsh)
 
 
 # Install Oh My Zsh
-echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if test ! $(ls ~/.oh-my-zsh); then
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    echo "Oh My Zsh already installed, skipped."
+fi
 
+# Oh-my-zsh theme setup
+# Simplerich
+# https://github.com/philip82148/simplerich-zsh-theme?tab=readme-ov-file#install
+# cd path/to/where_u_want_to_clone_the_repo
+if test ! $(ls ~/.zsh-theme/); then
+    git clone --recursive https://github.com/philip82148/simplerich-zsh-theme ~/.zsh-theme/
+    cp ~/.zsh-theme/simplerich-zsh-theme/simplerich.zsh-theme ~/.oh-my-zsh/themes/
+fi
+
+# Oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+if test ! $(which neovim); then
+    brew install neovim
+fi
+
+if test! $(which fzf); then
+    brew install fzf
+fi
