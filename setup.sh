c7a22e3999923cba8d4efb66cfcf70b302c4b3b6
kaioteoi <15633941+kaioteoi@users.noreply.github.com>
2024-06-17 19:38:23 -0300

    Enhance setup.sh

--- setup.sh
+++ setup.sh
@@ -5,19 +5,21 @@
 # Install if we don't have it
 if test ! $(which brew); then
   echo "Installing Homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 # Make sure we’re using the latest Homebrew.
 brew update
-
+brew install chezmoi
 brew install coreutils
 
-brew install zsh
-chsh -s $(which zsh)
-
+# region ZSH
+if [ ! $(which zsh) ]; then
+    brew install zsh
+    chsh -s $(which zsh)
+fi
 
 # Install Oh My Zsh
-if test ! $(ls ~/.oh-my-zsh); then
+if [ ! -d ~/.oh-my-zsh ]; then
     echo "Installing Oh My Zsh..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
@@ -28,21 +30,43 @@ fi
 # Simplerich
 # https://github.com/philip82148/simplerich-zsh-theme?tab=readme-ov-file#install
 # cd path/to/where_u_want_to_clone_the_repo
-if test ! $(ls ~/.zsh-theme/); then
+if [ ! -d ~/.zsh-theme ]; then
     git clone --recursive https://github.com/philip82148/simplerich-zsh-theme ~/.zsh-theme/
-    cp ~/.zsh-theme/simplerich-zsh-theme/simplerich.zsh-theme ~/.oh-my-zsh/themes/
+    cp ~/.zsh-theme/simplerich.zsh-theme ~/.oh-my-zsh/themes/
 fi
 
 # Oh-my-zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 
-if test ! $(which neovim); then
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions ]; then
+    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+fi
+
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search ]; then
+    git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+fi
+# endregion ZSH
+
+if [ ! $(which nvim) ]; then
     brew install neovim
 fi
 
-if test! $(which fzf); then
+if [ ! $(which fzf) ]; then
     brew install fzf
 fi
+
+# region Alacritty
+if [ ! $(mdfind "kMDItemKind == 'Application'" | grep /Applications/Alacritty.app) ]; then
+    brew install --cask alacritty
+fi
+
+if [ ! -f ~/.config/alacritty/catpuccin-mocha.toml ]; then
+    curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
+fi
+# endregion
