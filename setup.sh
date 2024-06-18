bc592cf4895394b97bc10ad93a2ca378fadb5548
kaioteoi <15633941+kaioteoi@users.noreply.github.com>
2024-06-18 19:35:50 -0300

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,52 +1,80 @@
 #!/usr/bin/env bash
 
+# Colors
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT_GRAY='\033[0;37m'
+DARK_GRAY='\033[1;30m'
+LIGHT_RED='\033[1;31m'
+LIGHT_GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHT_BLUE='\033[1;34m'
+LIGHT_PURPLE='\033[1;35m'
+LIGHT_CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+RESET='\033[0m'
 
 # Check for Homebrew,
 # Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing Homebrew..."
+if [ ! $(which brew) ]; then
+  echo -e "${GREEN}Installing Homebrew...${RESET}"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 # Make sure we’re using the latest Homebrew.
+
 brew update
-brew install chezmoi
 brew install coreutils
+
+# region chezmoi
+if [ ! $(which chezmoi) ]; then
+    echo -e "${GREEN}Installing Chezmoi...${RESET}"
+    brew install chezmoi
+fi
+# endregion
 
 # region ZSH
 if [ ! $(which zsh) ]; then
+    echo -e "${GREEN}Installing ZSH...${RESET}"
     brew install zsh
     chsh -s $(which zsh)
 fi
 
-# Install Oh My Zsh
 if [ ! -d ~/.oh-my-zsh ]; then
-    echo "Installing Oh My Zsh..."
+    echo -e "${GREEN}Installing Oh My Zsh...${RESET}"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
-    echo "Oh My Zsh already installed, skipped."
+    echo -e "${ORANGE}Oh My Zsh already installed, skipped.${RESET}"
 fi
 
+ZSH_CUSTOM_PATH=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+ZSH_PATH=${ZSH:-~/.oh-my-zsh}
+
 # Oh-my-zsh theme setup
-if [ ! -d "$ZSH_CUSTOM/themes/spaceship-prompt" ]; then
-    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+if [ ! -d "$ZSH_CUSTOM_PATH/themes/spaceship-prompt/" ]; then
+    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM_PATH/themes/spaceship-prompt/" --depth=1
+    ln -s "$ZSH_CUSTOM_PATH/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM_PATH/themes/spaceship.zsh-theme"
 fi
 
 # Oh-my-zsh plugins
-if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [ ! -d "$ZSH_CUSTOM_PATH/plugins/zsh-autosuggestions/" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM_PATH/plugins/zsh-autosuggestions/"
 fi
 
-if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ ! -d "$ZSH_CUSTOM_PATH/plugins/zsh-syntax-highlighting/" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM_PATH/plugins/zsh-syntax-highlighting/"
 fi
 
-if [ ! -d ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions ]; then
-    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+if [ ! -d "$ZSH_CUSTOM_PATH/plugins/zsh-completions/" ]; then
+    git clone https://github.com/zsh-users/zsh-completions "$ZSH_CUSTOM_PATH/plugins/zsh-completions/"
 fi
 
-if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search ]; then
-    git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+if [ ! -d "$ZSH_CUSTOM_PATH/plugins/zsh-history-substring-search/" ]; then
+    git clone https://github.com/zsh-users/zsh-history-substring-search "$ZSH_CUSTOM_PATH/plugins/zsh-history-substring-search/"
 fi
 # endregion ZSH
 
@@ -67,7 +95,7 @@ if [ ! -f ~/.config/alacritty/catpuccin-mocha.toml ]; then
     curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
 fi
 
-if [ ! $(ls -R ~/Library/Fonts | grep ttf) ]; then
+if [ $(ls ~/Library/Fonts | grep -c FiraCode) -eq 0 ]; then
     brew install font-fira-code-nerd-font
 fi
 # endregion
