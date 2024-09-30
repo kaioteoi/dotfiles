8ec2c9fc65836ab917fb99cdf9d743c5acca7b1c
kaioteoi <15633941+kaioteoi@users.noreply.github.com>
2024-09-30 18:56:17 -0300

    Minor nvim tweaks

--- private_dot_config/nvim/lua/krandom/autocmds.lua
+++ private_dot_config/nvim/lua/krandom/autocmds.lua
@@ -1,7 +1,11 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
+local augroup = vim.api.nvim_create_autocmd
+local LocalGroup = augroup("LocalGroup", {})
+
 autocmd("LspAttach", {
+	group = LocalGroup,
 	callback = function(e)
 		local opts = { buffer = e.buf }
 		vim.keymap.set("n", "gd", function()
