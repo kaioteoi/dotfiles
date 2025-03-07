393bd48793aafd291439e0a98546b09fd34d0721
kaioteoi <15633941+kaioteoi@users.noreply.github.com>
2025-03-06 21:58:34 -0300

    chore(zshrc/nvim): minor updates

--- private_dot_config/nvim/lua/krandom/autocmds.lua
+++ private_dot_config/nvim/lua/krandom/autocmds.lua
@@ -1,7 +1,6 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
-local augroup = vim.api.nvim_create_autocmd
 local LocalGroup = augroup("LocalGroup", {})
 
 autocmd("LspAttach", {
